b4a93e6fe1e14a330603f94656dd2964a08c5fc7
saleh700 <saleh700069@gmail.com>
2024-03-05 00:22:39 +0300

    4th commit

---
+++ front-end/src/constants/index.js
@@ -0,0 +1,9 @@
+export const daysOfWeek = [
+    {title: "Sunday", key: 1, shortcut: 'sun' },
+    {title: "Monday", key: 2, shortcut: 'mon' },
+    {title: "Tuesday", key: 3, shortcut: 'tue' },
+    {title: "Wednesday", key: 4, shortcut: 'wed' },
+    {title: "Thursday", key: 5, shortcut: 'thu' },
+    {title: "Friday", key: 6, shortcut: 'fri' },
+    {title: "Saturday", key: 7, shortcut: 'stur' },
+]
