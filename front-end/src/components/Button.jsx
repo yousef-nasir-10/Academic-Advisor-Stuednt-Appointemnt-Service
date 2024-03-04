d88445067475ff8adabd3907f02915f03c359c9d
yosuf <yosuf.nasir.10@hotmail.com>
2024-03-04 19:05:43 +0300

    5th commit

--- front-end/src/components/Button.jsx
+++ front-end/src/components/Button.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const Button = ({ label, bgColor, txtColor, addedStyles }) => {
+const Button = ({ label,  addedStyles, handleClick  }) => {
   return (
-    <button className={`${bgColor} ${txtColor} w-3/6    p-2 rounded-md hover:bg-[#0f515a] ${addedStyles}`}> {label}
+    <button className={` w-3/6 p-2 rounded-md hover:bg-[#0f515a] ${addedStyles}`} onClick={handleClick}> 
+      {label}
     </button>
   )
 }
