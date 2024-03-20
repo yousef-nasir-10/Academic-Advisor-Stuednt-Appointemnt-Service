af7d3d4b802eb9312200cea7f5f4e2fb9e2b0c96
yosuf <yosuf.nasir.10@hotmail.com>
2024-03-20 06:18:38 +0300

    16t hcommit

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,33 +1,124 @@
-import { useEffect, useState, Fragment, useRef } from 'react'
-import Button from '../components/Button'
-import { useNavigate } from 'react-router-dom'
-import { GetAllDoctors } from '../API/doctor'
+import { useEffect, useState } from 'react'
+import { GetDoctorById } from '../API/doctor'
 import { message } from 'antd'
 import { DoctorCard } from '../components/DoctorCard'
-import { Dialog, Transition } from '@headlessui/react'
-import { CheckIcon } from '@heroicons/react/24/outline'
 import DialogCom from '../components/DialogCom'
-import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import moment from 'moment';
-import Datepicker from "react-tailwindcss-datepicker";
+import { Box, sliderClasses } from '@mui/material'
+import { BookAppointment, GetDoctorAppointments, GetDoctorAppointmentsOnDate, GetUserAppointments } from '../API/appointments'
+import dayjs from 'dayjs'
+import { ArrowDownIcon, ArrowUpIcon } from '@heroicons/react/20/solid'
+
 
 const Home = () => {
 
-  const [doctors, setDoctors] = useState([])
   const [open, setOpen] = useState(false)
-  const cancelButtonRef = useRef(null)
+  const [doctor, setDoctor] = useState(null)
+  const [date, setDate] = useState(null)
+  const [selectedSloat, setSelectedSloat] = useState("")
+  const [bookedSloats, setBookedSloats] = useState([])
+  const [isSubbmit, setIsSubbmit] = useState(false)
+  const [status, setStatus] = useState("")
+  const [sesstion, setSesstion] = useState({
+    pending: [],
+    approved: [],
+    canceled: []
+  })
+
+  const [formData, SetFormData] = useState({
+    reason: "",
+    via: ""
+  })
   const [value, setValue] = useState({
     startDate: null,
     endDate: null
-});
+  });
 
 
+
+  useEffect(() => {
+    getData()
+  }, [])
+  useEffect(() => {
+    if (date) {
+      getBookedSlots()
+    }
+
+  }, [date])
+
+  useEffect(() => {
+    getSettions()
+
+
+  }, [status])
+
+  useEffect(() => {
+    if (selectedSloat && formData.reason != "" && formData.via != "") {
+      setIsSubbmit(true)
+    } else {
+      setIsSubbmit(false)
+    }
+
+  }, [selectedSloat, formData.reason, formData.via])
+
+  const getSettions = async () => {
+    const user = JSON.parse(localStorage.getItem("user"))
+    console.log(user);
+    if (user.role === "advisor") {
+      const response = await GetDoctorAppointments(user.id)
+      console.log(response);
+      if (response.success) {
+        setSesstion(response.data)
+        console.log(response);
+      }
+    } else {
+      const response = await GetUserAppointments(user.id)
+      console.log(response);
+      if (response.success) {
+        const pending = response.data.filter(sesstion => sesstion.status === "pending")
+        const approved = response.data.filter(sesstion => sesstion.status === "approved")
+        const canceled = response.data.filter(sesstion => sesstion.status === "cancelled")
+        console.log(approved);
+        setSesstion({
+          pending,
+          approved,
+          canceled
+        })
+        
+
+      }
+    }
+  }
+
+
+
+
+  function handleChange(event) {
+
+    const { name, value, type, checked } = event.target
+    SetFormData(prevStat => {
+      return {
+        ...prevStat,
+        [name]: type === "checkbox" ? checked : value,
+
+      }
+
+    })
+    console.log(formData);
+  }
   const getData = async () => {
     try {
-      const response = await GetAllDoctors()
+      const user = JSON.parse(localStorage.getItem("user"))
+      console.log(user);
+      const response = await GetDoctorById(user.advisedBy)
       if (response.success) {
-        console.log(response.data);
-        setDoctors(response.data)
+        console.log(response);
+        setDoctor(response.data)
+
       } else {
         message.error(response.message)
       }
@@ -35,20 +126,135 @@ const Home = () => {
       message.error(error.message)
     }
   }
-  const handleValueChange = (newValue) => {
-    console.log("newValue:", newValue.startDate);
-    setDate(newValue.startDate);
-    setValue(newValue)
-}
 
-  useEffect(() => {
-    getData()
-  }, [])
+  const getSloatData = () => {
+    const day = moment(date).format("dddd")
+    if (!doctor.days.includes(day)) {
+      return <h3>Doctor is not available on {moment(date).format("YYYY-MM-DD")}</h3>
+    }
 
-  const navigate = useNavigate()
+    let startTime = moment(doctor.startTime, "HH:mm")
+    let endTime = moment(doctor.endTime, "HH:mm")
+
+    let sloatDuration = 30 // in minutes 
+    const slots = []
+    const booked = []
+    const a = []
+
+
+
+    while (startTime < endTime) {
+      // if (bookedSloats?.find((slot) => slot.slot === startTime.format("HH:mm"))) {
+      //     booked.push(startTime.format("HH:mm"))
+
+      // }
+      // if (!bookedSloats?.find((slot) => slot.slot === startTime.format("HH:mm"))) {
+      //     slots.push(startTime.format("HH:mm"))
+
+      // }
+      slots.push(startTime.format("HH:mm"))
+
+
+      startTime.add(sloatDuration, "minutes")
+    }
+
+
+    console.log(a);
+
+    return (
+      <>
+        {slots.map((slot, index) => {
+          const isBooked = bookedSloats?.find(
+            (bookedSloat) => bookedSloat.slot === slot && bookedSloat.status !== 'cancelled'
+          )
+          return (
+
+
+
+            <div
+              className={
+                ` p-2 rounded-md  bg-[#19774025]
+                          ${selectedSloat === slot ? "bg-[#1d7e9e6b]" : ""}
+                          ${isBooked ? "pointer-events-none bg-[#77191925] " : ""}
+                          `
+              }
+              onClick={() => {
+
+                setSelectedSloat(slot)
+              }}
+
+            >
+              <span>{moment(slot, "HH:mm ").format("HH:mm ")}   - {moment(slot, "HH:mm").add(sloatDuration, "minutes").format("HH:mm ")}</span>
+            </div>
+
+
+          )
+        })}
+
+
+      </>
+    )
+  }
+
+  const getBookedSlots = async () => {
+    const user = JSON.parse(localStorage.getItem("user"))
+    console.log(user);
+    try {
+      const response = await GetDoctorAppointmentsOnDate(user.advisedBy, date)
+      console.log(response);
+      if (response.success) {
+        setBookedSloats(response.data)
+      } else {
+        message.error(response.message)
+      }
+
+    } catch (error) {
+      message.error(error.message)
+    }
+  }
+  const onBookAppointment = async () => {
+    try {
+      const payload = {
+        doctorId: doctor.id,
+        userId: JSON.parse(localStorage.getItem("user")).id,
+        date,
+        slot: selectedSloat,
+        doctorName: `${doctor.firstName} ${doctor.lastName}`,
+        userName: JSON.parse(localStorage.getItem("user")).username,
+        bookedOn: dayjs().format("DD-MM-YYYY HH:mm "),
+        reason: formData.reason,
+        via: formData.via,
+        status: "pending"
+      }
+      const response = await BookAppointment(payload)
+      if (message.success) {
+        message.success(response.message)
+        // navigate('/profile')
+      } else {
+        message.error(response.message)
+      }
+    } catch (error) {
+      message.error(error.message)
+    }
+  }
+
+  const notificationMethods = [
+    { id: 'inPerson', title: 'Face to Face ', },
+    { id: 'phone', title: 'Phone call' },
+    { id: 'online', title: 'Online (Zoom)' },
+  ]
+  const stats = [
+    { name: 'Upcoming sesstion', stat: sesstion.approved, previousStat: '70,946', change: '12%', changeType: 'increase' },
+    { name: 'Pending Sesstion', stat: sesstion.pending, previousStat: '56.14%', change: '2.02%', changeType: 'increase' },
+    { name: 'Canceled Sesstion', stat: sesstion.canceled, previousStat: '28.62%', change: '4.05%', changeType: 'decrease' },
+  ]
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
+
   return (
     <div className='student-page flex flex-col   '>
-
+      {/* advisor info */}
       <div className='flex flex-col w-full p-4  mb-2  ' >
         <h1 className='text-xl '>Your Academic advisor:</h1>
         <DoctorCard
@@ -58,16 +264,10 @@ const Home = () => {
           email="Mustafa@gmail.com"
           phone="96685798765"
           office="2050"
-
-
         />
-
-
-
-
       </div>
+      {/* secuduling a sesstion section  */}
       <div className='flex w-full '>
-
         <div className='flex w-full h-[300px] p-4 bg-[#46cc2500] items-center justify-center flex-col'>
           <div className='flex md:w-[700px] mb-4'>
 
@@ -86,39 +286,97 @@ const Home = () => {
           <DialogCom open={open} onClose={() => setOpen(false)}>
 
             <div className='flex flex-col w-full'>
-              <div>
-                <div>
+              <div className='flex flex-col w-full'>
+                <div className=' mt-4'>
+                  <Box>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                      <DemoContainer components={['DatePicker']}>
+                        <DatePicker
+                          label="Today's sesstions"
+                          value={value}
+                          minDate={dayjs()}
 
-                  <Datepicker
-                    value={value}
-                    onChange={handleValueChange}
-                    asSingle={true}
-                    minDate={moment().format("YYYY-MM-DD")}
-                    classNames="outline-none "
-                    showShortcuts={true}
-                    primaryColor={"sky"}
+                          onChange={(newValue) => {
+                            setDate(dayjs(newValue).format("YYYY-MM-DD"))
+                            setSelectedSloat(null)
+                            console.log(dayjs(newValue).format("YYYY-MM-DD"));
+                          }}
 
-                    configs={{
-                      shortcuts: {
-                        today: "Today",
-                      },
 
-                    }}
+                        />
+                      </DemoContainer>
+                    </LocalizationProvider>
+                  </Box>
+                  <div className="grid mt-2 grid-cols-2 gap-3 sm:grid-cols-3 lg:grid-cols-3">
 
-                  />
+                    {doctor && getSloatData()}
+                  </div>
+                  <div className='mt-2'>
+                    <label className="text-base font-semibold text-gray-900">meduim </label>
+                    <p className="text-sm text-gray-500">How do you prefer to conduct this sesstion?</p>
+                    <fieldset className="mt-4">
+                      <legend className="sr-only">Notification method</legend>
+                      <div className="space-y-4 sm:flex sm:items-center sm:space-x-10 sm:space-y-0">
+                        {notificationMethods.map((notificationMethod) => (
+                          <div key={notificationMethod.id} className="flex items-center">
+                            <input
+                              id={notificationMethod.id}
+                              name="via"
+                              type="radio"
+                              value={notificationMethod.id}
+                              onChange={handleChange}
+                              defaultChecked={notificationMethod.id === 'email'}
+                              className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                            />
+                            <label htmlFor={notificationMethod.id} className="ml-3 block text-sm font-medium leading-6 text-gray-900">
+                              {notificationMethod.title}
+                            </label>
+                          </div>
+                        ))}
+                      </div>
+                    </fieldset>
+                  </div>
+
+                  <div className='mt-2'>
+                    <label htmlFor="comment" className="block text-sm font-medium leading-6 text-gray-900">
+                      Write a message
+                    </label>
+                    <div className="mt-2">
+                      <textarea
+                        rows={4}
+                        onChange={handleChange}
+                        name="reason"
+                        value={formData.reason}
+                        id="comment"
+                        className="block p-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 outline-none max-h-20"
+
+                        placeholder=''
+                      />
+                    </div>
+                  </div>
+
+
+
+
                 </div>
+
+
               </div>
-              <div className="flex w-full items-center  bg-slate-50 justify-between">
-                <div className=" w-full flex items-center justify-start  gap-x-6 bg-slate-500">
+
+              <div className="flex w-full items-center  bg justify-between">
+                <div className=" w-full flex items-center justify-start  gap-x-6 mt-4">
                   <button type="button" onClick={() => { setOpen(false) }} className="text-sm font-semibold leading-6 mr-2 text-gray-900">
                     Cancel
                   </button>
                   <button
                     type="submit"
-                    onClick={() => { setOpen(false) }}
-                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    onClick={() => {
+                      onBookAppointment()
+                      setOpen(false)
+                    }}
+                    className={`${!isSubbmit ? " pointer-events-none bg-[#77191925] px-4 py-2 rounded-md cursor-not-allowed opacity-50" : "rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"}`}
                   >
-                    Save
+                    Seudule
                   </button>
                 </div>
               </div>
@@ -132,20 +390,43 @@ const Home = () => {
 
 
       </div>
-
       {/* second row */}
+
+
       <div className='flex w-full mt-2 '>
-        <div className='flex w-3/6 bg-slate-50 p-4 h-[300px] bg-[#cc842515] mr-2' >
-          <h1>Upcoming Sesstions </h1>
+        <div className='flex w-full flex-col  '>
+          <h3 className="text-base font-semibold leading-6 text-gray-900">Last 30 days</h3>
+          <dl className="mt-5 grid grid-cols-1 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow md:grid-cols-3 md:divide-x md:divide-y-0">
+            {stats.map((item) => (
+              <div key={item.name} className="px-4 py-5 sm:p-6">
+                <dt className="text-base font-normal text-gray-900">{item.name}</dt>
+                <dd className="mt-1 flex items-baseline justify-between md:block lg:flex">
+                  {item.stat.map((sesstion, index) => (
+                    <div>
+                    <div className="flex items-baseline text-md font-semibold text-indigo-600">
+                      
+                    {sesstion.date}
+                    <span className="ml-2 text-sm font-medium text-gray-500">at {sesstion.slot}</span>
+                  </div>
+                  <div className="flex items-baseline text-md font-semibold text-black/80">
+                      
+                      Way of conduction:
+                      <span className="ml-2 text-sm font-medium text-gray-500"> {sesstion.via}</span>
+                    </div>
 
+                  </div>
+                  
+                  ))}
+
+                </dd>
+
+
+                
+              </div>
+            ))}
+          </dl>
         </div>
-        <div className='flex w-3/6 bg-slate-100 p-4 bg-[#cc25250c] h-auto'>
-          <h1>Canceled sesstions </h1>
-
-        </div>
-
       </div>
-
 
     </div>
   )
